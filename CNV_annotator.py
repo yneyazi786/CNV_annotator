92dac87c6c23afd906511b6fce00d915abf56657
Yasser <139060693+yneyazi786@users.noreply.github.com>
2025-10-26 15:09:34 +0530

    Change label color to #00e5ff in CNV annotator

--- CNV_annotator.py
+++ CNV_annotator.py
@@ -143,9 +143,9 @@ st.markdown(
     unsafe_allow_html=True
 )
 annotator = HGVSCNVAnnotator("cytoBand.txt", "Genelist.csv")
-st.markdown("<label style='font-size:18px; font-weight:600; color:#fff;'>Enter coordinate (e.g., chr16:15489724-16367962)</label>", unsafe_allow_html=True)
+st.markdown("<label style='font-size:18px; font-weight:600; color:#00e5ff;'>Enter coordinate (e.g., chr16:15489724-16367962)</label>", unsafe_allow_html=True)
 coordinate = st.text_input("Enter coordinate (e.g., chr16:15489724-16367962)", label_visibility="collapsed")
-st.markdown("<label style='font-size:18px; font-weight:600; color:#fff;'>Select event type</label>", unsafe_allow_html=True)
+st.markdown("<label style='font-size:18px; font-weight:600; color:#00e5ff;'>Select event type</label>", unsafe_allow_html=True)
 event_type = st.selectbox("Select event type", ["duplication", "deletion"], label_visibility="collapsed")
 
 if st.button("Annotate"):
